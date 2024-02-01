5f9f9102df741479287cb27a23ce200acdab17ee
tarou-samurai <kiri.4115@gmail.com>
2024-02-01 17:56:17 +0900

    deploy to heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\App;
+ use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +27,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Paginator::useBootstrap();
+        if(App::environment(['production'])) {
+            URL::forceScheme('https');
+        }
     }
 }
